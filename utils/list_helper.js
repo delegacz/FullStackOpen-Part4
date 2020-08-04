d0bfb7c814d17f0be364e9ad4d89847dc3ee5c9e
Mateusz Delegacz <mat.delegacz@gmail.com>
2020-08-04 21:39:49 +0100

    4.3 completed

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = () => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    return blogs.reduce((sum, blog) => {
+        return sum + blog.likes
+    },0)
+}
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
